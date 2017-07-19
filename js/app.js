2d4546df0ab3d9207165497cf69fb44eddb7fde1
harvey.zh <iamzhaihy@users.noreply.github.com>
2017-07-19 00:18:50 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,17 +2,17 @@
 
 // Model for this application
 var fullAttractionList = [
-    {title: "University of Tokyo", location: {lat: 35.712678, lng: 139.761989}, type: ["school/college"], qualified: false},
-    {title: "Tokyo Skytree", location: {lat: 35.710063, lng: 139.8107}, type: ["observation-tower", "landmark"], qualified: false},
-    {title: "Tokyo Tower", location: {lat: 35.65857, lng: 139.745484}, type: ["observation-tower", "landmark", "dating"], qualified: false},
-    {title: "Pokemon Center", location: {lat: 35.728798, lng: 139.719247}, type: ["shopping", "fun", "souvenir"], qualified: false},
-    {title: "Ueno Zoo", location: {lat: 35.716454, lng: 139.771318}, type: ["zoo", "park", "fun"], qualified: false},
-    {title: "Akihabara", location: {lat: 35.6984949, lng: 139.7719771}, type: ["shopping", "fun", "souvenir"], qualified: false},
-    {title: "Shinjuku Gyoen National Garden", location: {lat: 35.6851763, lng: 139.707863}, type: ["park", "garden"], qualified: false},
-    {title: "Sensō-ji", location: {lat: 35.7147651, lng: 139.7944666}, type: ["temple"], qualified: false}, 
-    {title: "Meiji Jingu", location: {lat: 35.675512, lng: 139.7007245}, type: ["shrine", "garden"], qualified: false},
-    {title: "Tokyo Disneyland", location: {lat: 35.6328964, lng: 139.8782056}, type: ["fun", "park", "souvenir"], qualified: false},
-    {title: "Ghibli Museum", location: {lat: 35.696238, lng: 139.568243}, type: ["fun", "museum", "souvenir"], qualified: false}
+    {title: "University of Tokyo", location: {lat: 35.712678, lng: 139.761989}, type: ["school/college"]},
+    {title: "Tokyo Skytree", location: {lat: 35.710063, lng: 139.8107}, type: ["observation-tower", "landmark"]},
+    {title: "Tokyo Tower", location: {lat: 35.65857, lng: 139.745484}, type: ["observation-tower", "landmark", "dating"]},
+    {title: "Pokemon Center", location: {lat: 35.728798, lng: 139.719247}, type: ["shopping", "fun", "souvenir"]},
+    {title: "Ueno Zoo", location: {lat: 35.716454, lng: 139.771318}, type: ["zoo", "park", "fun"]},
+    {title: "Akihabara", location: {lat: 35.6984949, lng: 139.7719771}, type: ["shopping", "fun", "souvenir"]},
+    {title: "Shinjuku Gyoen National Garden", location: {lat: 35.6851763, lng: 139.707863}, type: ["park", "garden"]},
+    {title: "Sensō-ji", location: {lat: 35.7147651, lng: 139.7944666}, type: ["temple"]}, 
+    {title: "Meiji Jingu", location: {lat: 35.675512, lng: 139.7007245}, type: ["shrine", "garden"]},
+    {title: "Tokyo Disneyland", location: {lat: 35.6328964, lng: 139.8782056}, type: ["fun", "park", "souvenir"]},
+    {title: "Ghibli Museum", location: {lat: 35.696238, lng: 139.568243}, type: ["fun", "museum", "souvenir"]}
     //{title:, location: {lat:, lng:}, type:}
 ];
 
